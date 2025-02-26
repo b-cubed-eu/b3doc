c8f9078edcd884a37a9ea981ccd0b784ea25a2c6
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-02-26 15:43:12 +0100

    Create test-rmd_to_md.R

---
+++ tests/testthat/test-rmd_to_md.R
@@ -0,0 +1,27 @@
+test_that("rmd_to_md() writes .md to a directory", {
+  skip_if_offline()
+  rmd_file <- "https://raw.githubusercontent.com/b-cubed-eu/gcube/refs/heads/main/vignettes/articles/occurrence-process.Rmd"
+  output_dir <- "r/gcube"
+
+  figures_dir <- file.path("public", output_dir)
+  markdown_dir <- file.path("src", "content", "docs", output_dir)
+
+  on.exit(unlink(figures_dir, recursive = TRUE))
+  on.exit(unlink(markdown_dir, recursive = TRUE))
+
+  rmd_to_md(rmd_file, output_dir)
+
+  expect_identical(
+    list.files(figures_dir),
+    c(
+      "occurrence-process-unnamed-chunk-12-1.png",
+      "occurrence-process-unnamed-chunk-3-1.png",
+      "occurrence-process-unnamed-chunk-7-1.png",
+      "occurrence-process-unnamed-chunk-9-1.png"
+      )
+    )
+  expect_identical(
+    list.files(markdown_dir),
+    c("occurrence-process.md")
+  )
+})
