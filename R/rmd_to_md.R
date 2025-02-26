d7eae5bdb1c54a1ed9da33942be5c075a6aa7d5f
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-02-26 12:02:58 +0100

    Create rmd_to_md.R
    Co-Authored-By: Peter Desmet <peter.desmet@inbo.be>

---
+++ R/rmd_to_md.R
@@ -0,0 +1,45 @@
+rmd_to_md <- function(rmd_file, output_dir) {
+  md_name <- gsub(".Rmd$", "", basename(rmd_file))
+
+  # Set input
+  if (grepl("^http", rmd_file)) {
+    input_file <- file.path(tempdir(), basename(rmd_file))
+    download.file(
+      rmd_file,
+      input_file
+    )
+  } else {
+    input_file <- rmd_file
+  }
+
+  # Set output
+  figures_dir <- file.path("public", output_dir)
+  figures_dir_url <- paste0("/astro-docs/", output_dir, "/")
+  markdown_dir <- file.path("src", "content", "docs", output_dir)
+  markdown_file <- file.path(markdown_dir, paste0(md_name, ".md"))
+
+  # Set options
+  knitr::opts_knit$set(
+    progress = TRUE,
+    # Directory for figures
+    base.dir = figures_dir,
+    # Path to figures in markdown
+    base.url = figures_dir_url
+  )
+  knitr::opts_chunk$set(
+    # Subdirectory for figures, default figure/
+    fig.path = paste(md_name, "-"),
+    # Captions for figures, "" will keep existing, but not create new ones
+    fig.cap = ""
+  )
+
+  # Knit
+  knitr::knit(
+    input = input_file,
+    output = markdown_file
+  )
+
+  # TODO: reset knitr opts_knit
+  # TODO: empty tempdir
+  invisible(file)
+}
