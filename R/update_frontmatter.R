e7b0dd6bba5156ac226e58830f79ede73c4dd9ed
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-03-10 15:00:42 +0100

    align parameter names with `rmd_to_md()`

--- R/update_frontmatter.R
+++ R/update_frontmatter.R
@@ -2,26 +2,26 @@
 #'
 #' Updates the front matter of a markdown file on disk.
 #'
-#' @param file_path Path to the markdown file on disk.
-#' @param original_file_path URL of the R Markdown file, either a local path or a URL.
+#' @param md_file_path Path to the markdown file on disk.
+#' @param rmd_file Path to the R Markdown file, either a local path or a URL.
 #' @param order Order of the article in the menu.
 #' @return Markdown file with updated front matter, written to disk.
 #' @noRd
 #' @examples
-#' file_path <- here::here("output", "src", "content", "docs", "software", "gcube", "occurrence-process.md")
-#' original_file_path <- "https://raw.githubusercontent.com/b-cubed-eu/gcube/refs/heads/main/vignettes/articles/occurrence-process.Rmd"
+#' md_file_path <- here::here("output", "src", "content", "docs", "software", "gcube", "occurrence-process.md")
+#' rmd_file <- "https://raw.githubusercontent.com/b-cubed-eu/gcube/refs/heads/main/vignettes/articles/occurrence-process.Rmd"
 #' order <- 1
-#' update_frontmatter(file_path, original_file_path, order)
-update_frontmatter <- function(file_path, original_file_path, order) {
+#' update_frontmatter(md_file_path, rmd_file, order)
+update_frontmatter <- function(md_file_path, rmd_file, order) {
   # Check that the required arguments are present
   if (missing(order)) {stop("The 'order' argument is required.")}
 
   # Transform original file path from raw to edit mode
-  original_file_path <- gsub("raw.githubusercontent.com", "github.com", original_file_path)
-  original_file_path <- gsub("/refs/heads/", "/blob/", original_file_path)
+  rmd_file <- gsub("raw.githubusercontent.com", "github.com", rmd_file)
+  rmd_file <- gsub("/refs/heads/", "/blob/", rmd_file)
 
   # Read front matter
-  lines <- readLines(file_path)
+  lines <- readLines(md_file_path)
   frontmatter_start <- which(lines == "---")[1]
   frontmatter_end <- which(lines == "---")[2]
   frontmatter_char <- lines[(frontmatter_start + 1):(frontmatter_end - 1)]
@@ -30,7 +30,7 @@ update_frontmatter <- function(file_path, original_file_path, order) {
   # Update front matter
   frontmatter$lastUpdated <- format(Sys.time(), "%Y-%M-%d")
   frontmatter$sidebar$order <- order
-  frontmatter$source <- original_file_path
+  frontmatter$source <- rmd_file
   new_frontmatter <- yaml::as.yaml(frontmatter)
 
   # Write front matter
@@ -41,5 +41,5 @@ update_frontmatter <- function(file_path, original_file_path, order) {
   )
 
   # Write the updated file
-  writeLines(updated_lines, file_path)
+  writeLines(updated_lines, md_file_path)
 }
