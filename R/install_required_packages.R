e99cc3966661390cd923014d7fb6d7bbd357b425
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-03-11 16:42:23 +0100

    export function + use b-cubed-r-universe repo

--- R/install_required_packages.R
+++ R/install_required_packages.R
@@ -1,3 +1,11 @@
+#' Install required packages
+#'
+#' Installs or updates the packages used in an R Markdown file.
+#'
+#' @param input_file Path to the R Markdown file
+#'
+#' @returns A character vector with the names of the packages used
+#' @export
 install_required_packages <- function(input_file) {
   # Read the Rmd file from the URL
   rmd_content <- readLines(input_file, warn = FALSE)
@@ -11,9 +19,12 @@ install_required_packages <- function(input_file) {
   # Install or update packages
   for (pkg in package_names) {
     if (!require(pkg, character.only = TRUE)) {
-      install.packages(pkg)
+      utils::install.packages(pkg, repos = c('https://b-cubed-eu.r-universe.dev', 'https://cloud.r-project.org'))
     } else {
-      update.packages(pkg, ask = FALSE)
+      utils::update.packages(pkg, repos = c('https://b-cubed-eu.r-universe.dev', 'https://cloud.r-project.org'))
     }
   }
+
+  # Return the list of packages used
+  return(package_names)
 }
